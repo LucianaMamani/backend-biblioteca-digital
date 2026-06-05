4a4a4ab805e2a0c18dce990b42cc61ad75388447
Marianoabizanda <mariano.abizanda@gmail.com>
2026-06-05 00:55:01 -0300

    Agrego views y urls con endpoints REST

--- biblioteca_digital/api/urls.py
+++ biblioteca_digital/api/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register(r'autores', views.AutorViewSet)
+router.register(r'generos', views.GeneroViewSet)
+router.register(r'libros', views.LibroViewSet)
+router.register(r'usuarios', views.UsuarioViewSet)
+router.register(r'reservas', views.ReservaViewSet)
+
 urlpatterns = [
-    # Se van a agregar los endpoints acá
+    path('', include(router.urls)),
 ]
--- biblioteca_digital/api/views.py
+++ biblioteca_digital/api/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Autor, Genero, Libro, Usuario, Reserva
+from .serializers import AutorSerializer, GeneroSerializer, LibroSerializer, UsuarioSerializer, ReservaSerializer
 
-# Create your views here.
+
+class AutorViewSet(viewsets.ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+
+
+class GeneroViewSet(viewsets.ModelViewSet):
+    queryset = Genero.objects.all()
+    serializer_class = GeneroSerializer
+
+
+class LibroViewSet(viewsets.ModelViewSet):
+    queryset = Libro.objects.all()
+    serializer_class = LibroSerializer
+
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+
+class ReservaViewSet(viewsets.ModelViewSet):
+    queryset = Reserva.objects.all()
+    serializer_class = ReservaSerializer
